efb7604cda1dd90e560e7ccbe238c7e171f28e22
marii9 <116024194+marii9@users.noreply.github.com>
2023-02-19 21:11:52 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,12 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+     id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT  AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
